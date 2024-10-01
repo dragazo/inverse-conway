0fc7f5bdd11777c3eed2eed9cde1f2d808b95739
Devin Jean <emailcruzjean@yahoo.com>
2024-10-01 05:54:18 -0500

    backward

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@
 use core::fmt;
 use alloc::vec::Vec;
 
+use z3::{Context, Solver, SatResult};
+use z3::ast::{Ast, Bool, Int};
+
 #[derive(Clone, PartialEq, Eq)]
 pub struct Conway {
     rows: usize,
@@ -35,20 +38,85 @@ impl Conway {
             Err(())
         }
     }
-    pub fn forward(&self) -> Self {
-        let mut counts = vec![0; self.rows * self.cols];
+    pub fn inflate(&self, padding: usize) -> Self {
+        let mut res = Self::new(self.rows + 2 * padding, self.cols + 2 * padding);
         for row in 0..self.rows {
             for col in 0..self.cols {
-                if self.data[row * self.cols + col] {
-                    for c_row in row.saturating_sub(1)..(row + 2).min(self.rows) {
-                        for c_col in col.saturating_sub(1)..(col + 2).min(self.cols) {
-                            counts[c_row * self.cols + c_col] += 1;
+                res.set(row + padding, col + padding, self.get(row, col).unwrap()).unwrap();
+            }
+        }
+        res
+    }
+    pub fn forward(&self, steps: usize) -> Self {
+        let mut res = self.clone();
+        for _ in 0..steps {
+            let mut counts = vec![0; res.rows * res.cols];
+            for row in 0..res.rows {
+                for col in 0..res.cols {
+                    if res.data[row * res.cols + col] {
+                        for c_row in row.saturating_sub(1)..(row + 2).min(res.rows) {
+                            for c_col in col.saturating_sub(1)..(col + 2).min(res.cols) {
+                                counts[c_row * res.cols + c_col] += 1;
+                            }
                         }
                     }
                 }
             }
+            res = Conway { rows: res.rows, cols: res.cols, data: res.data.iter().zip(counts).map(|(live, count)| (*live && count == 4) || count == 3).collect() };
         }
-        Self { rows: self.rows, cols: self.cols, data: self.data.iter().zip(counts).map(|(live, count)| (*live && count == 4) || count == 3).collect() }
+        res
+    }
+    pub fn backward(&self, steps: usize) -> Option<Self> {
+        let context = Context::new(&Default::default());
+
+        let zero = Int::from_u64(&context, 0);
+        let one = Int::from_u64(&context, 1);
+        let three = Int::from_u64(&context, 3);
+        let four = Int::from_u64(&context, 4);
+
+        let vars = {
+            let mut vars = Vec::with_capacity(self.rows * self.cols);
+            for row in 0..self.rows {
+                for col in 0..self.cols {
+                    vars.push(Bool::new_const(&context, format!("d{row},{col}")));
+                }
+            }
+            vars
+        };
+
+        let mut res = vars.clone();
+        for _ in 0..steps {
+            let mut counts = vec![zero.clone(); self.rows * self.cols];
+            for row in 0..self.rows {
+                for col in 0..self.cols {
+                    for c_row in row.saturating_sub(1)..(row + 2).min(self.rows) {
+                        for c_col in col.saturating_sub(1)..(col + 2).min(self.cols) {
+                            counts[c_row * self.cols + c_col] += res[row * self.cols + col].ite(&one, &zero);
+                        }
+                    }
+                }
+            }
+            res = res.iter().zip(counts).map(|(live, count)| (live & count._eq(&four) | count._eq(&three))).collect();
+        }
+
+        let s = Solver::new(&context);
+        for (res, actual) in res.iter().zip(&self.data) {
+            if *actual {
+                s.assert(res);
+            } else {
+                let res = res.not();
+                s.assert(&res);
+            }
+        }
+
+        match s.check() {
+            SatResult::Sat => {
+                let model = s.get_model().unwrap();
+                Some(Self { rows: self.rows, cols: self.cols, data: vars.iter().map(|v| model.eval(v, true).unwrap().as_bool().unwrap()).collect() })
+            }
+            SatResult::Unsat => None,
+            SatResult::Unknown => unreachable!(),
+        }
     }
 }
 impl fmt::Debug for Conway {
--- tests/forward.rs
+++ tests/forward.rs
@@ -1,20 +1,46 @@
 use inverse_conway::Conway;
 
+pub fn parse(s: &str) -> Option<Conway> {
+    let lines = s.lines().map(|x| x.trim()).filter(|x| !x.is_empty()).collect::<Vec<_>>();
+    let (rows, cols) = (lines.len(), lines[0].chars().count());
+    if lines.iter().any(|x| x.chars().count() != cols) {
+        return None;
+    }
+
+    let mut res = Conway::new(rows, cols);
+    for (row, line) in lines.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            res.set(row, col, match ch {
+                '■' => true,
+                '□' => false,
+                _ => return None,
+            }).unwrap();
+        }
+    }
+    Some(res)
+}
+
 #[test]
-fn test_forward_block() {
+fn test_new() {
+    assert_eq!(Conway::new(0, 4), Conway::new(0, 0));
+    assert_eq!(Conway::new(4, 0), Conway::new(0, 0));
+}
+
+#[test]
+fn test_block() {
     let mut start = Conway::new(4, 4);
     start.set(1, 1, true).unwrap();
     start.set(1, 2, true).unwrap();
     start.set(2, 1, true).unwrap();
     start.set(2, 2, true).unwrap();
-    let next_1 = start.forward();
-    let next_2 = next_1.forward();
+    let next_1 = start.forward(1);
+    let next_2 = next_1.forward(1);
     assert_eq!(start, next_1);
     assert_eq!(start, next_2);
 }
 
 #[test]
-fn test_forward_beehive() {
+fn test_beehive() {
     let mut start = Conway::new(5, 6);
     start.set(1, 2, true).unwrap();
     start.set(1, 3, true).unwrap();
@@ -22,14 +48,14 @@ fn test_forward_beehive() {
     start.set(2, 4, true).unwrap();
     start.set(3, 2, true).unwrap();
     start.set(3, 3, true).unwrap();
-    let next_1 = start.forward();
-    let next_2 = next_1.forward();
+    let next_1 = start.forward(1);
+    let next_2 = next_1.forward(1);
     assert_eq!(start, next_1);
     assert_eq!(start, next_2);
 }
 
 #[test]
-fn test_forward_loaf() {
+fn test_loaf() {
     let mut start = Conway::new(6, 6);
     start.set(1, 2, true).unwrap();
     start.set(1, 3, true).unwrap();
@@ -38,41 +64,41 @@ fn test_forward_loaf() {
     start.set(3, 2, true).unwrap();
     start.set(3, 4, true).unwrap();
     start.set(4, 3, true).unwrap();
-    let next_1 = start.forward();
-    let next_2 = next_1.forward();
+    let next_1 = start.forward(1);
+    let next_2 = next_1.forward(1);
     assert_eq!(start, next_1);
     assert_eq!(start, next_2);
 }
 
 #[test]
-fn test_forward_boat() {
+fn test_boat() {
     let mut start = Conway::new(5, 5);
     start.set(1, 1, true).unwrap();
     start.set(1, 2, true).unwrap();
     start.set(2, 1, true).unwrap();
     start.set(2, 3, true).unwrap();
     start.set(3, 2, true).unwrap();
-    let next_1 = start.forward();
-    let next_2 = next_1.forward();
+    let next_1 = start.forward(1);
+    let next_2 = next_1.forward(1);
     assert_eq!(start, next_1);
     assert_eq!(start, next_2);
 }
 
 #[test]
-fn test_forward_tub() {
+fn test_tub() {
     let mut start = Conway::new(5, 5);
     start.set(1, 2, true).unwrap();
     start.set(2, 1, true).unwrap();
     start.set(2, 3, true).unwrap();
     start.set(3, 2, true).unwrap();
-    let next_1 = start.forward();
-    let next_2 = next_1.forward();
+    let next_1 = start.forward(1);
+    let next_2 = next_1.forward(1);
     assert_eq!(start, next_1);
     assert_eq!(start, next_2);
 }
 
 #[test]
-fn test_forward_blinker() {
+fn test_blinker() {
     let mut start = Conway::new(5, 5);
     start.set(1, 2, true).unwrap();
     start.set(2, 2, true).unwrap();
@@ -81,12 +107,20 @@ fn test_forward_blinker() {
     next_1.set(2, 1, true).unwrap();
     next_1.set(2, 2, true).unwrap();
     next_1.set(2, 3, true).unwrap();
-    assert_eq!(start.forward(), next_1);
-    assert_eq!(next_1.forward(), start);
+    assert_eq!(start.forward(1), next_1);
+    assert_eq!(next_1.forward(1), start);
+    assert_eq!(start.forward(2), next_1.forward(1));
+    assert_eq!(start.forward(4), next_1.forward(1));
+
+    assert_eq!(next_1.backward(0).unwrap(), next_1);
+    let prev_1 = next_1.backward(1).unwrap();
+    assert_eq!(prev_1.forward(1), next_1);
+    let prev_2 = next_1.backward(2).unwrap();
+    assert_eq!(prev_2.forward(2), next_1);
 }
 
 #[test]
-fn test_forward_toad() {
+fn test_toad() {
     let mut start = Conway::new(6, 6);
     start.set(2, 2, true).unwrap();
     start.set(2, 3, true).unwrap();
@@ -101,12 +135,20 @@ fn test_forward_toad() {
     next_1.set(2, 1, true).unwrap();
     next_1.set(3, 1, true).unwrap();
     next_1.set(4, 2, true).unwrap();
-    assert_eq!(start.forward(), next_1);
-    assert_eq!(next_1.forward(), start);
+    assert_eq!(start.forward(1), next_1);
+    assert_eq!(next_1.forward(1), start);
+    assert_eq!(start.forward(2), next_1.forward(1));
+    assert_eq!(start.forward(4), next_1.forward(1));
+
+    assert_eq!(next_1.backward(0).unwrap(), next_1);
+    let prev_1 = next_1.backward(1).unwrap();
+    assert_eq!(prev_1.forward(1), next_1);
+    let prev_2 = next_1.backward(2).unwrap();
+    assert_eq!(prev_2.forward(2), next_1);
 }
 
 #[test]
-fn test_forward_beacon() {
+fn test_beacon() {
     let mut start = Conway::new(6, 6);
     start.set(1, 1, true).unwrap();
     start.set(1, 2, true).unwrap();
@@ -123,6 +165,56 @@ fn test_forward_beacon() {
     next_1.set(3, 4, true).unwrap();
     next_1.set(4, 3, true).unwrap();
     next_1.set(4, 4, true).unwrap();
-    assert_eq!(start.forward(), next_1);
-    assert_eq!(next_1.forward(), start);
+    assert_eq!(start.forward(1), next_1);
+    assert_eq!(next_1.forward(1), start);
+    assert_eq!(start.forward(2), next_1.forward(1));
+    assert_eq!(start.forward(4), next_1.forward(1));
+
+    assert_eq!(next_1.backward(0).unwrap(), next_1);
+    let prev_1 = next_1.backward(1).unwrap();
+    assert_eq!(prev_1.forward(1), next_1);
+    let prev_2 = next_1.backward(2).unwrap();
+    assert_eq!(prev_2.forward(2), next_1);
+}
+
+#[test]
+fn test_big() {
+    let start = parse(r#"
+□□□■■□□■■□□■■□□□
+□□■□■□□■■□□■□■□□
+□■□□□□□□□□□□□□■□
+■□□□□□□□□□□□□□□■
+■■□□■■■□□□□□□□■■
+□□□□□□□□□■□□□□□□
+□□□□□□□□□■□□□□□□
+□□□□□□□□□■□□□□□□
+■■□□■■■□□□□□□□■■
+■□□□□□□□□□□□□□□■
+□■□□□□□□□□□□□□■□
+□□■□■□□■■□□■□■□□
+□□□■■□□■■□□■■□□□
+"#).unwrap();
+    let next_1 = parse(r#"
+□□□■■□□■■□□■■□□□
+□□■□■□□■■□□■□■□□
+□■□□□□□□□□□□□□■□
+■□□□□■□□□□□□□□□■
+■■□□□■□□□□□□□□■■
+□□□□□■□□□□□□□□□□
+□□□□□□□□■■■□□□□□
+□□□□□■□□□□□□□□□□
+■■□□□■□□□□□□□□■■
+■□□□□■□□□□□□□□□■
+□■□□□□□□□□□□□□■□
+□□■□■□□■■□□■□■□□
+□□□■■□□■■□□■■□□□
+"#).unwrap();
+    assert_eq!(start.forward(1), next_1);
+    assert_eq!(next_1.forward(1), start);
+
+    assert_eq!(start.backward(0).unwrap(), start);
+    let prev_1 = start.backward(1).unwrap();
+    assert_eq!(prev_1.forward(1), start);
+    let prev_2 = start.backward(2).unwrap();
+    assert_eq!(prev_2.forward(2), start);
 }
